bb4c2af63dc0d8a9866fe4f730ba238d3926e986
inoveAlumnos <alumnos@inovecode.com>
2024-12-08 21:18:44 -0300

    Ejercicios practica

---
+++ ejercicios_practica/ejercicio_2.py
@@ -0,0 +1,39 @@
+# Numpy [Python]
+# Ejercicios de práctica
+
+# Autor: Inove Coding School
+# Version: 2.0
+
+# IMPORTANTE: NO borrar los comentarios
+# que aparecen en verde con el hashtag "#"
+
+# Ejercicios con lambda
+
+
+if __name__ == '__main__':
+    print("Bienvenidos a otra clase de Inove con Python")
+    
+    # Lambda expression
+    # 1)
+    # Realizar una funcion lambda que retorne el tamaño
+    # (len) de un string pasado como parámetro
+
+    # len_string = lambda......
+
+    # 2)
+    # Lista de string
+    palabras = ['Inove', 'casa', 'programacion']
+
+    # Utilice la función map para mapear una lambda expression
+    # que retorne el tamaño (len) de cada texto em cata iteración
+    # de la lista de textos
+    # El resultado (el len de cada palabra) se debe ir almacenando
+    # en una nueva lista
+    # Nota: realizar la lambda expression "in line"
+    # Copiar la lambda creada en el paso anterior dentro del map
+    # NOTA: No debe usar "len_string" dentro del map, debe colocar
+    # directamente la lambda.
+
+    # palabras_len = list(map....)
+
+    print("terminamos")
---
+++ ejercicios_practica/ejercicio_3.py
@@ -0,0 +1,48 @@
+# Numpy [Python]
+# Ejercicios de práctica
+
+# Autor: Inove Coding School
+# Version: 2.0
+
+# IMPORTANTE: NO borrar los comentarios
+# que aparecen en verde con el hashtag "#"
+
+# Ejercicios con comprensión de listas
+
+
+if __name__ == '__main__':
+    print("Bienvenidos a otra clase de Inove con Python")
+    
+    # Práctica de comprensión de listas
+    # 1)
+    # Generar una lista a partir de comprensión de listas,
+    # esta lista generada deberá tener un tamaño de 11
+    # números, conteniendo del 0 al 10 inclusive
+
+    # lista_0_10 = [......]
+
+    # 2)
+    # Generar una lista a partir de comprensión de listas,
+    # esta lista generada deberá contener la tabla del 5,
+    # desde el múltiplo 0 al múltiplo 10
+    # El resultado esperado es:
+    # [0 5 10 15 20 25 30 35 40 45 50]
+    # Utilizar comprensión de listas para generar essa lista
+    # Lo esperable es que realicen una lista de 11 elementos,
+    # del 0 al 10 (como el ejer anterior) pero que cada
+    # elemento lo multipliquen x5.
+
+    # tabla_5 = [......]
+
+    # 3)
+    # Generar una lista a partir de comprensión de listas,
+    # esta lista generada deberá contener 10 números aleatorios,
+    # estos números deberán estar entre el rango 1 al 30 representando
+    # números posibles de un mes (los números pueden repetirse).
+    # NOTA: Importar el módulo random y utilizar randrange
+    # o randint para generar números aleatorios.
+    # https://docs.python.org/3/library/random.html
+
+    # dias_mes = [.....]
+
+    print("terminamos")
